11ca4ceef913305ee5f7928c909c30fca5d8e0c8
alexchri234 <alex.kimiti@gmail.com>
2026-08-23 01:46:41 +0200

    app.py

--- app.py
+++ app.py
@@ -154,12 +154,12 @@ if video_uploadee is not None:
             import cv2
             import math
             import os
-    if not os.path.exists("pose_landmarker.task"):
-        import urllib.request
-        urllib.request.urlretrieve(
-            "https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/1/pose_landmarker_lite.task",
-            "pose_landmarker.task"
-    )
+            if not os.path.exists("pose_landmarker.task"):
+                import urllib.request
+                urllib.request.urlretrieve(
+                    "https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/1/pose_landmarker_lite.task",
+                    "pose_landmarker.task"
+                )
 
             base_options = python.BaseOptions(model_asset_path='pose_landmarker.task')
             options = vision.PoseLandmarkerOptions(
